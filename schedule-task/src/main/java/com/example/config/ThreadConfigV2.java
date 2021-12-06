6a329d26e6c9cdce2d6795b7b42fcae8a8d838cb
hujesse83 <jesse.hu@liveramp.com>
2021-12-06 15:28:36 +0800

    spring schedule with async

--- schedule-task/src/main/java/com/example/config/ThreadConfigV2.java
+++ schedule-task/src/main/java/com/example/config/ThreadConfigV2.java
@@ -1,7 +1,9 @@
 package com.example.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -10,22 +12,19 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @Author huJesse
- * @Date 2021/12/6 14:20
- * @Description
- * @Configuration用于定义配置类，被注解的类内部包含有一个或多个被@Bean注解的方法， 这些方法将会被AnnotationConfigApplicationContext或AnnotationConfigWebApplicationContext类进行扫描，并用于构建bean定义，初始化Spring容器。
- * @EnableAsync开始对异步任务的支持
+ * @Date 2021/12/6 14:49
  */
 @Configuration
 @EnableAsync
-public class ThreadConfig {
+public class ThreadConfigV2 implements AsyncConfigurer {
     private static final int corePoolSize = 10;            // 核心线程数（默认线程数）
     private static final int maxPoolSize = 100;                // 最大线程数
     private static final int keepAliveTime = 10;            // 允许线程空闲时间（单位：默认为秒）
     private static final int queueCapacity = 200;            // 缓冲队列数
-    private static final String threadNamePrefix = "Async-Service-"; // 线程池名前缀
+    private static final String threadNamePrefix = "basic-service-"; // 线程池名前缀
 
-    @Bean(name = "MyPool")
-    public Executor createExecutor() {
+    @Override
+    public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(corePoolSize);
         executor.setMaxPoolSize(maxPoolSize);
@@ -34,16 +33,13 @@ public class ThreadConfig {
         executor.setThreadNamePrefix(threadNamePrefix);
         // 线程池对拒绝任务的处理策略
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        /**
-         * 其中我们主要注意的就是拒绝策略方法：setRejectedExecutionHandler（），拒绝策略常用有有这四种
-         *
-         * ThreadPoolExecutor.AbortPolicy 丢弃任务并抛出RejectedExecutionException异常(默认)。
-         * ThreadPoolExecutor.DiscardPolic 丢弃任务，但是不抛出异常。
-         * ThreadPoolExecutor.DiscardOldestPolicy 丢弃队列最前面的任务，然后重新尝试执行任务
-         * ThreadPoolExecutor.CallerRunsPolic 由调用线程处理该任务
-         */
         // 初始化
         executor.initialize();
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
     }
 }
--- schedule-task/src/main/java/com/example/schedulingtasks/ScheduledTasks.java
+++ schedule-task/src/main/java/com/example/schedulingtasks/ScheduledTasks.java
@@ -5,8 +5,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * @Author huJesse
@@ -16,201 +14,19 @@ import java.util.Date;
 @Component
 public class ScheduledTasks {
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     static {
-        log.info("ScheduledTasks begins");
+        log.info("ScheduledTasks with async begins");
     }
 
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
+    @Scheduled(fixedDelay = 1000)
+    @Async("MyPool")  // 指定线程池的名称
     public void reportCurrentTime2() {
         log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
     }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCurrentTime3() {
+    @Scheduled(fixedDelay = 1000)
+    @Async  // 不指定线程池的名称
+    public void reportCurrentTime() {
         log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
     }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCvurrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCudrrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportfCurrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCurraentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCurrenvtTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCurrentTidme() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCurrent2Time() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCurrent3Time() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCurr2entTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportC2urrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void repo2rtCurrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void rep21ortCurrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void rep2ortCurrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCur5rentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportC4urrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCur0rentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCu9rrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCur8rentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCu7rrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportC6urrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void reportCur7rentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @Async("MyPool")
-
-    public void repor6tCurrentTime() {
-        log.info(Thread.currentThread().getName());
-        log.info("The time is now {}", dateFormat.format(new Date()));
-    }
-
 }
