daa99921110b929f21c6bfc6fd39e071ffff1d7f
hujesse83 <jesse.hu@liveramp.com>
2022-02-09 11:33:52 +0800

    bigDecimal

---
+++ random/src/main/java/BigDecimalTest.java
@@ -0,0 +1,32 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+/**
+ * @author jesse.hu
+ * @date 2022/2/9 11:23
+ * add 方法用于将两个 BigDecimal 对象相加，
+ * subtract 方法用于将两个 BigDecimal 对象相减。
+ * multiply 方法用于将两个 BigDecimal 对象相乘，
+ * divide 方法用于将两个 BigDecimal 对象相除。
+
+ */
+public class BigDecimalTest {
+    public static void main(String[] args) {
+        BigDecimal a = new BigDecimal("1.0");
+        BigDecimal b = new BigDecimal("0.9");
+        BigDecimal c = new BigDecimal("0.8");
+        BigDecimal d = new BigDecimal("0.5");
+        BigDecimal x = a.subtract(b);
+        BigDecimal y = b.subtract(c);
+
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+        System.out.println(a.divide(d,6, RoundingMode.HALF_UP));
+
+        if (x.compareTo(y)==0){
+            System.out.println("true");
+        }
+        System.out.println(Objects.equals(x,y));
+    }
+}
